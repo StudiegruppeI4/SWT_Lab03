8b0369f3f7f608da4972ec97bdb8488df1905738
nikolaj808 <45153292+nikolaj808@users.noreply.github.com>
2020-02-12 14:33:50 +0100

    Refactored design implemented

---
+++ ECS.Legacy/IHeater.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    public interface IHeater
+    {
+        void TurnOn();
+        void TurnOff();
+        bool RunSelfTest();
+    }
+
+    public class Heater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+
+    public class FakeHeater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
---
+++ ECS.Legacy/ITempSensor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    public interface ITempSensor
+    {
+        int GetTemp();
+        bool RunSelfTest();
+    }
+
+    public class TempSensor : ITempSensor
+    {
+        private Random gen = new Random();
+
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+
+    public class FakeTempSensor : ITempSensor
+    {
+        private Random gen = new Random();
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
